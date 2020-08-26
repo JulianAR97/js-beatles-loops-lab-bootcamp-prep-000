db24cef626a55a8e896983bb6747fdd6e6f8ba41
whimsical-logger-3153 <julianrosenthal@yahoo.com>
2020-08-26 00:09:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var musicianToInstrument = [];
+
+  for (var i = 0; i < musicians.length; i++) {
+    var musician = musicians[i];
+    var instrument = instruments[i];
+
+    var musicInstrument = `${musician} plays ${instrument}`;
+
+    musicianToInstrument.push(musicInstrument);
+  }
+  return musicianToInstrument;
+}
